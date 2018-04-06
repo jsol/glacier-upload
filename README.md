658f8a9338dec0afb164a587905777092fc954b4
Jens Olsson <jens@rootsy.nu>
2018-04-06 21:03:42 +0200

    Add some help text and README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # glacier-upload
+
+The purpose is to provide a class that implements a Node writable stream
+that can upload data of arbitrary size to Amazon Glacier.
+
+It splits the data into chunks and performs a multi-upload while using the
+stream to keep RAM usage as low as possible.
+
+Default configuration should send approximatly three requests at the time to
+Glacier.
+
+## Todo
+More tests
+Re-upload individual chunks if something goes wrong.
+Resume failed uploads
+A mode where it can clean out old options.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+if (!process.env.AWS_SDK_LOAD_CONFIG) {
+  // Fetch the sdk config if not told otherwise.
+  process.env.AWS_SDK_LOAD_CONFIG = 'true'
+}
+
+const argv = require('minimist')(process.argv.slice(2))
+
+if (argv.h || argv.help) {
+  console.log(`
+    Upload a file of any size to Glacier.
+    Use environment variables to set the AWS credentials, for example
+    AWS_SDK_LOAD_CONFIG=true to use the ones stored in the home folder.
+
+    The following flags are mandatory:
+
+    --fileName, -f\t The file to upload
+    --vaultName, -n\t The name of the vault to upload to
+
+    The following flags are optional
+    --progress, -p\t Show a progress bar for this upload
+    --jsonFile, -j\t Store the archive information in this JSON file.
+                     Will create a new file if the path does not exist,
+                     and will add to the existing info if it already exists.
+                     Saving the archive information locally makes archive
+                     retrieval or deletion a lot quicker.
+  `)
+  process.exit(0)
+}
 const GlacierStream = require('./GlacierStream')
 const fs = require('fs')
 const ProgressBar = require('progress')
-const argv = require('minimist')(process.argv.slice(2))
 
 const vaultName = argv.vaultName || argv.n || null
 const fileName = argv.fileName || argv.f || null
